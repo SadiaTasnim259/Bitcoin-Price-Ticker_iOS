ac634d52a80461f4df78029bb9b5127a8cceded4
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-13 22:51:08 +0600

    outlet added

--- Bitcoin Price Ticker_iOS/ViewController.swift
+++ Bitcoin Price Ticker_iOS/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var bitCoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
