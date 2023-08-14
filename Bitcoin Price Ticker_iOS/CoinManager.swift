d78f7fe6816da2e39d12e2fab653ea43301656e4
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-14 23:30:58 +0600

    API key created

--- Bitcoin Price Ticker_iOS/CoinManager.swift
+++ Bitcoin Price Ticker_iOS/CoinManager.swift
@@ -10,4 +10,8 @@ import Foundation
 struct CoinManager{
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(){
+        
+    }
 }
--- Bitcoin Price Ticker_iOS/URLConstant.swift
+++ Bitcoin Price Ticker_iOS/URLConstant.swift
@@ -7,4 +7,8 @@
 
 import Foundation
 
-
+enum API{
+    static let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC/"
+    static let apiKey = "5002C846-5A81-4740-BBE6-4CDF5EC8F866"
+    
+}
