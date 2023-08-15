462d31d528b606acc63ccfca250790b2cc77d6fa
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-15 22:29:17 +0600

    protocol , delegate created

--- Bitcoin Price Ticker_iOS/Screens/ViewModel/CoinCurrencyViewModel.swift
+++ Bitcoin Price Ticker_iOS/Screens/ViewModel/CoinCurrencyViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+protocol CoinCurrencyDelegate {
+    func showBitCoinRate(rate:Double?)
+    func showBitCoinRateError(error: Error)
+}
+
+class CoinCurrencyModel{
+    var delegate: CoinCurrencyDelegate?
+    
+    func getBitCoinRate(currency:String){
+        let URL = API.baseURL+currency+"?apikey="+API.apiKey
+        NetworkManager.shared.httpRequest(urlString: URL, httpMethodType: .GET, respnseType: CoinModel.self) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let success):
+                    self.delegate?.showBitCoinRate(rate: success.rate)
+                case .failure(let failure):
+                    self.delegate?.showBitCoinRateError(error: failure)
+                }
+            }
+        }
+    }
+}
